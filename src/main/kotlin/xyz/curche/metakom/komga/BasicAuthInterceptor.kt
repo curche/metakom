879dab1ad09fd629ad9ea390f3e828c0d866e77e
curche <72807749+curche@users.noreply.github.com>
2021-12-23 00:08:27 +0530

    Move okhttp helper functions to separate files

---
+++ src/main/kotlin/xyz/curche/metakom/komga/BasicAuthInterceptor.kt
@@ -0,0 +1,23 @@
+package xyz.curche.metakom.komga
+
+import okhttp3.Credentials
+import okhttp3.Interceptor
+import okhttp3.Response
+import java.io.IOException
+import kotlin.jvm.Throws
+
+class BasicAuthInterceptor(id: String, pass: String): Interceptor {
+    var credentials: String
+
+    init {
+        credentials = Credentials.basic(id, pass)
+    }
+
+    @Throws(IOException::class)
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val oldRequest = chain.request()
+        val newRequestBuilder = oldRequest.newBuilder()
+            .header("Authorization", credentials)
+        return chain.proceed(newRequestBuilder.build())
+    }
+}
--- src/main/kotlin/xyz/curche/metakom/komga/komga.kt
+++ src/main/kotlin/xyz/curche/metakom/komga/komga.kt
@@ -2,73 +2,14 @@ package xyz.curche.metakom.komga
 
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
-import okhttp3.*
+import okhttp3.Request
+import okhttp3.Response
+import okhttp3.Headers
+import okhttp3.OkHttpClient
 import xyz.curche.metakom.config.Config
-import java.io.IOException
+import xyz.curche.metakom.network.GET
 import java.lang.IllegalStateException
 import java.util.concurrent.TimeUnit
-import kotlin.jvm.Throws
-
-private val DEFAULT_CACHE_CONTROL = CacheControl.Builder().maxAge(10, TimeUnit.MINUTES).build()
-private val DEFAULT_HEADERS = Headers.Builder().build()
-private val DEFAULT_BODY: RequestBody = FormBody.Builder().build()
-const val DEFAULT_USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36 Edg/88.0.705.63"
-
-fun GET(
-    url: String,
-    headers: Headers = DEFAULT_HEADERS,
-    cache: CacheControl = DEFAULT_CACHE_CONTROL
-): Request {
-    return Request.Builder()
-        .url(url)
-        .headers(headers)
-        .cacheControl(cache)
-        .build()
-}
-
-fun POST(
-    url: String,
-    headers: Headers = DEFAULT_HEADERS,
-    body: RequestBody = DEFAULT_BODY,
-    cache: CacheControl = DEFAULT_CACHE_CONTROL
-) : Request {
-    return Request.Builder()
-        .url(url)
-        .post(body)
-        .headers(headers)
-        .cacheControl(cache)
-        .build()
-}
-
-fun PATCH(
-    url: String,
-    headers: Headers = DEFAULT_HEADERS,
-    body: RequestBody = DEFAULT_BODY,
-    cache: CacheControl = DEFAULT_CACHE_CONTROL
-): Request {
-    return Request.Builder()
-        .url(url)
-        .patch(body)
-        .headers(headers)
-        .cacheControl(cache)
-        .build()
-}
-
-class BasicAuthInterceptor(id: String, pass: String): Interceptor {
-    lateinit var credentials: String
-
-    init {
-        credentials = Credentials.basic(id, pass)
-    }
-
-    @Throws(IOException::class)
-    override fun intercept(chain: Interceptor.Chain): Response {
-        val oldRequest = chain.request()
-        val newRequestBuilder = oldRequest.newBuilder()
-            .header("Authorization", credentials)
-        return chain.proceed(newRequestBuilder.build())
-    }
-}
 
 open class Komga(config: Config) {
 
