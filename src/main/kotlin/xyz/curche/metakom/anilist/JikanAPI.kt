7cad7e7f4fccb58e7b6ba337459097a9d506f9a9
curche <72807749+curche@users.noreply.github.com>
2022-01-03 17:48:24 +0530

    Get Magazine where manga is serialized (if possible)
    MyAnimeList contains serialization magazine info for most manga that
    they have while Anilist does not. Using the Jikan API for MAL and
    corresponding MAL ID for an Anilist Entry (if it exists), we try to
    fetch the serialization details.

---
+++ src/main/kotlin/xyz/curche/metakom/anilist/JikanAPI.kt
@@ -0,0 +1,36 @@
+package xyz.curche.metakom.anilist
+
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.jsonArray
+import kotlinx.serialization.json.jsonObject
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import xyz.curche.metakom.network.GET
+import java.util.concurrent.TimeUnit
+
+class JikanAPI {
+
+    private val client = OkHttpClient.Builder()
+        .addInterceptor(AnilistRateLimitInterceptor(permits = 3, period = 1, unit = TimeUnit.SECONDS))
+        .build()
+
+    private val json by lazy { Json { ignoreUnknownKeys = true } }
+
+    fun getSerializationFromID(malId: Int): String {
+        val response = client.newCall(GET("$apiUrl/manga/$malId")).execute()
+        val responseBody = response.body?.string().orEmpty()
+        val responseBodyAsJson = json.decodeFromString<JsonObject>(responseBody)
+        val data = responseBodyAsJson["data"]!!.jsonObject
+        val media = data["serializations"]!!.jsonArray
+        return media
+            .map { it.jsonObject["name"] }
+            .joinToString(separator = ", ")
+    }
+
+    companion object {
+        private const val apiUrl = "https://api.jikan.moe/v4"
+        private val jsonMime = "application/json; charset=utf-8".toMediaType()
+    }
+}
